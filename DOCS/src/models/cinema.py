fd50a66d6bc1350025890d35e4bcdbf7c7aaa007
zambellissima <grazielly.fuzato@sou.unifeob.edu.br>
2026-05-07 21:03:43 -0300

    Create cinema.py

---
+++ DOCS/src/models/cinema.py
@@ -0,0 +1,9 @@
+class Cinema:
+
+    def __init__(self, id, nome, cidade, estado, endereco, capacidade):
+        self.id = id
+        self.nome = nome
+        self.cidade = cidade
+        self.estado = estado
+        self.endereco = endereco
+        self.capacidade = capacidade
