ce7c03efe69ec0695e4ebd25d31950228f71f49e
zambellissima <grazielly.fuzato@sou.unifeob.edu.br>
2026-05-07 21:04:24 -0300

    Create sessao.py

---
+++ DOCS/src/models/sessao.py
@@ -0,0 +1,9 @@
+class Sessao:
+
+    def __init__(self, id, data, horario, publico, cinema_id, filme_id):
+        self.id = id
+        self.data = data
+        self.horario = horario
+        self.publico = publico
+        self.cinema_id = cinema_id
+        self.filme_id = filme_id
