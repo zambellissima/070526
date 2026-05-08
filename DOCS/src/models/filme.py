da56d2d4bfbd73f5d1ee65b1bfec3765788597b8
zambellissima <grazielly.fuzato@sou.unifeob.edu.br>
2026-05-07 21:04:02 -0300

    Create filme.py

---
+++ DOCS/src/models/filme.py
@@ -0,0 +1,9 @@
+class Filme:
+
+    def __init__(self, id, titulo, genero, classificacao, duracao, diretor):
+        self.id = id
+        self.titulo = titulo
+        self.genero = genero
+        self.classificacao = classificacao
+        self.duracao = duracao
+        self.diretor = diretor
